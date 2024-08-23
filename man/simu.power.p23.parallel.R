e916f9d901e8e804c3ba5688575ab19aacb3574d
phe9480 <99561631+phe9480@users.noreply.github.com>
2024-08-22 20:16:05 -0400

    Add files via upload

---
+++ man/simu.power.p23.parallel.R
@@ -0,0 +1,208 @@
+#' Phase 2/3 Inferential Seamless Design Power calculation by simulations
+#'
+#' This functions calculates the cumulative power and overall power for a group sequential design by simulations.
+#'
+#' @param nSim Number of simulated trials
+#' @param n1 Stage 1 sample size of each dose and control arm. length(n1) must be number of arms.
+#' @param n2 Stage 2 Sample size of the selected dose and control arm. length(n2) must be 2.
+#' @param m Median survival time for each arm (dose 1, dose 2, ..., control). length(m) must be equal to length(n1)
+#' @param A1 Enrollment period for Stage 1
+#' @param Lambda1 Enrollment distribution function (CDF) for stage 1.
+#' @param DCO1 Data cutoff date for Stage 1
+#' @param enrollment.hold Holding period in months after DCO1 of Stage 1 prior to enrollment of Stage 2 patients. 0 means seamless enrollment.
+#' @param A2 Enrollment period for Stage 2
+#' @param Lambda2 Enrollment distribution function (CDF) for stage 2.
+#' @param targetEvents2 Planned target number of events for Stage 2. Either targetEvents2 must be provided. 
+#' @param alpha Type I error (one-sided) for testing the selected dose, usually 0.025.
+#' @param sf Spending functions. acceptable options include all spending functions in gsDesign R package, for example, "gsDesign::sfLDOF"
+#' @param method Options include "Independent Incremental": z1 at dose selection and z2 is from dose selection to kth analysis at stage 2; 
+#' "Disjoint Subjects": z1 is at kth analysis for stage 1 subjects; z2 is at the kth analysis for stage 2 subjects. z1 will be adjusted by multiplicity and closed testing procedure at each analysis.
+#' "Mixture": Only consider disjoint subjects at first analysis in stage 2. Starting from the 2nd analysis, consider independent incremental methods. Only z1 at 1st analysis will be adjusted by multiplicity and closed testing procedure.
+#' @param nCore Number of cores distributed for simulation;
+#' @param seed An integer, or nCore number of integers as random seed for reproducibility;
+#' 
+#' @return An object with values:
+#' \describe{
+#' \item{bd.z}{z value rejection boundary at each analysis}
+#' \item{cum.pow}{Cumulative power}
+#' \item{s}{Selected dose}
+#' \item{selection}{Probability of selection among doses}
+#' }
+#' 
+#' 
+#' @examples
+#' #Example (1): Stage 1: 4 arms; 3 dose levels; each arm 50 patients.
+#' #Stage 2: additional 200 patients per arm will be enrolled at stage 2
+#' #medians for the 4 arms: 9, 11, 13 and control = 8 months
+#' #Enrollment: 12 months uniform in stage 1; 12 months uniform in stage 2
+#' #Holding period: 4 months between stage 1 and 2
+#' #Dose selection will be based on data cut at 16 months
+#' #Stage 2 has 2 planned analyses at 300 and 380 events respectively.
+#'
+#' #Dose selection decision is NOT based on ORR.
+#' simu.power.p23.parallel(nSim=10, n1 = rep(50, 4), n2 = rep(200, 2), m = c(9, 9, 9, 9), 
+#' orr = NULL, rho = NULL, dose_selection_endpoint = "not ORR",
+#' Lambda1 = function(t){(t/12)*as.numeric(t<= 12) + as.numeric(t > 12)}, A1 = 12,
+#' Lambda2 = function(t){(t/12)*as.numeric(t<= 12) + as.numeric(t > 12)}, A2 = 12,
+#' enrollment.hold=4, DCO1 = 16, targetEvents2=c(300, 380), sf=gsDesign::sfLDOF, 
+#' alpha=0.025, method = "Independent Incremental", nCore = 8)
+#' 
+#' #Example (2): #Dose selection decision based on ORR
+#' simu.power.p23.parallel(nSim=100000, n1 = rep(50, 4), n2 = rep(200, 4), m = c(9, 9, 9, 9), 
+#' orr = c(0.25, 0.3, 0.4, 0.2), rho = 0.7, dose_selection_endpoint = "ORR",
+#' Lambda1 = function(t){(t/12)*as.numeric(t<= 12) + as.numeric(t > 12)}, A1 = 12,
+#' Lambda2 = function(t){(t/12)*as.numeric(t<= 12) + as.numeric(t > 12)}, A2 = 12,
+#' enrollment.hold=4, DCO1 = 16, targetEvents2=c(300, 380), sf=gsDesign::sfLDOF, 
+#' alpha=0.025, method = "Independent Incremental", nCore = 10)
+#' 
+#' simu.power.p23.parallel(nSim=10, n1 = rep(50, 4), n2 = rep(200, 4), m = c(9, 9, 9, 9), 
+#' orr = c(0.25, 0.3, 0.3, 0.2), rho = 0.7, dose_selection_endpoint = "ORR",
+#' Lambda1 = function(t){(t/12)*as.numeric(t<= 12) + as.numeric(t > 12)}, A1 = 12,
+#' Lambda2 = function(t){(t/12)*as.numeric(t<= 12) + as.numeric(t > 12)}, A2 = 12,
+#' enrollment.hold=4, DCO1 = 16, targetEvents2=c(300, 380), sf=gsDesign::sfLDOF, 
+#' alpha=0.025, method = "Disjoint Subjects", nCore = 8)
+#' 
+#' @export 
+#' 
+# simu.power.p23.onecore(bd.z=2)
+
+simu.power.p23.parallel <- function(nSim=100, n1 = rep(50, 4), n2 = rep(200, 2), m = c(9,9, 9, 9), 
+                                    orr = c(0.25, 0.3, 0.4, 0.2), rho = 0.7, dose_selection_endpoint = "ORR",
+                                    Lambda1 = function(t){(t/12)*as.numeric(t<= 12) + as.numeric(t > 12)}, A1 = 12,
+                                    Lambda2 = function(t){(t/12)*as.numeric(t<= 12) + as.numeric(t > 12)}, A2 = 12,
+                                    enrollment.hold=4, DCO1 = 16, targetEvents2=c(300, 380), 
+                                    alpha=0.025, sf=gsDesign::sfLDOF, multiplicity.method="simes",
+                                    method = "Independent Incremental", nCore=NULL, seed=123){
+  
+  
+  simu.power.p23.onecore = function(seed, nSim=10, n1 = rep(50, 4), n2 = rep(200, 2), m = c(9,9, 9, 9), 
+                                    orr = c(0.25, 0.3, 0.4, 0.2), rho = 0.7, dose_selection_endpoint = "ORR",
+                                    Lambda1 = function(t){(t/12)*as.numeric(t<= 12) + as.numeric(t > 12)}, A1 = 12,
+                                    Lambda2 = function(t){(t/12)*as.numeric(t<= 12) + as.numeric(t > 12)}, A2 = 12,
+                                    enrollment.hold=4, DCO1 = 16, targetEvents2=c(300, 380), 
+                                    alpha=0.025, sf=gsDesign::sfLDOF, multiplicity.method="simes",
+                                    method = "Independent Incremental", bd.z=NULL){
+    
+    set.seed(seed)
+    #Number of analyses in stage 2
+    K = length(targetEvents2)
+    
+    #Number of arms
+    n.arms = length(n1)
+    
+    #rejection boundary by traditional GSD
+    if(is.null(bd.z)){
+      if (K == 1) {bd.z = qnorm(1-alpha)} else {
+        bd.z = gsDesign::gsDesign(k=K,alpha=alpha,timing=targetEvents2/targetEvents2[K],sfu=sf, test.type=1)$upper$bound
+      }
+    }
+    
+    
+    #Combination Z values
+    comb.z = matrix(NA, nrow=nSim, ncol=K)
+    s = rep(NA, nSim) #selected dose
+    
+    n2 = c(rep(n2[1], n.arms-1), n2[2])
+    for (i in 1:nSim){
+      p23i = simu.p23trial(n1 = n1, n2 = n2, m = m, 
+                           orr = orr, rho = rho, dose_selection_endpoint = dose_selection_endpoint,
+                           Lambda1 = Lambda1, A1 = A1, 
+                           Lambda2 = Lambda2, A2 = A2, enrollment.hold=enrollment.hold)
+      
+      o=conduct.p23(data=p23i, DCO1=DCO1, dose_selection_endpoint = dose_selection_endpoint, targetEvents2 = targetEvents2, method = method)
+      s[i] = o$s
+      
+      if (method == "Independent Incremental") {
+        for (j in 1:K){
+          oj = comb.pvalue.p23(z1=o$z1,  z2 = o$z2[,j], bd.z=bd.z[j], w=o$w[,j], selected.dose = s[i], method=multiplicity.method)
+          comb.z[i, j] = oj$comb.z; 
+        }
+      } else if (method == "Disjoint Subjects") {
+        for (j in 1:K){
+          oj = comb.pvalue.p23(z1=matrix(o$z1[j, ], nrow=1),  z2 = o$z2[,j], bd.z=bd.z[j], w=o$w[,j], selected.dose = s[i], method=multiplicity.method)
+          comb.z[i, j] = oj$comb.z; 
+        }
+      } else if (method == "Mixture") {
+        comb.z[i, ] = o$z.tilde
+      }
+    }
+    
+    # cum.pow=gsd.power(z = comb.z, bd.z=bd.z)
+    # 
+    # o = list()
+    # o$cum.pow = cum.pow
+    # o$bd.z = bd.z
+    
+    selection = rep(NA, n.arms-1)
+    for (j in 1:(n.arms-1)) {
+      selection[j] = sum(s == j) 
+    }
+    # o$selection = selection
+    # o$s=s
+    
+    re = list(comb.z=comb.z, selection=selection, s=s)
+    
+    return(re)
+  }
+  
+  
+  ## Start simulation 
+  if(is.null(nCore)){
+    nCore = max(parallel::detectCores()-4, 8)
+  }
+  #nsim_per_cluster <- 1000
+  cl <- parallel::makeCluster(nCore, type = "PSOCK")
+  doParallel::registerDoParallel(cl)
+  
+  nsim_per_cluster = ceiling(nSim/nCore)
+  
+  if(length(seed==1)) seed=(1:nCore)*seed
+  else if(length(seed)!=nCore) stop("The number of seeds should match the number of cores")
+  
+  
+  #Number of analyses in stage 2
+  K = length(targetEvents2)
+  
+  
+  if (K == 1) {bd.z = qnorm(1-alpha)} else {
+    bd.z = gsDesign::gsDesign(k=K,alpha=alpha,timing=targetEvents2/targetEvents2[K],sfu=sf, test.type=1)$upper$bound
+  }
+  
+  # Use parLapply to run in parallel
+  results <- parallel::parLapply(cl, seed, fun = simu.power.p23.onecore,
+                                 nSim=nsim_per_cluster,
+                                 n1 = n1, n2 = n2, m = m, 
+                                 orr = orr, rho = rho, dose_selection_endpoint = dose_selection_endpoint,
+                                 Lambda1 = Lambda1, A1 = A1,
+                                 Lambda2 = Lambda2, A2 = A2,
+                                 enrollment.hold=enrollment.hold, DCO1 = DCO1, targetEvents2=targetEvents2, 
+                                 alpha=alpha, sf=sf, multiplicity.method=multiplicity.method,
+                                 method = method, bd.z=bd.z
+  )
+  
+  comb.zall <- c()
+  select.all <- c()
+  s.all <- c()
+  for( i in 1:length(results)){
+    comb.zall = rbind(comb.zall, results[[i]]$comb.z)
+    select.all = rbind(select.all, results[[i]]$selection)
+    s.all <- c(s.all, results[[i]]$s)
+  }
+  
+  cum.pow=gsd.power(z = comb.zall, bd.z=bd.z)
+  
+  o = list()
+  o$cum.pow = cum.pow
+  o$bd.z = bd.z
+  
+  o$selection = colSums(select.all)/nSim
+  #o$s=s.all
+  
+  # Stop the cluster
+  parallel::stopCluster(cl)
+  
+  return(o)
+  
+}
+
+
